459ff50f56bd8cb60ec02d48b4bdd073221951ae
Sanjit Mann <sumatmann@gmail.com>
2022-01-30 23:37:12 -0800

    new

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express()
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
   app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('public/index'))
+  app.get('/', (req, res) => res.render('pages/index'))
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
