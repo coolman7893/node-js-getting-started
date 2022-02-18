b5c166a8180fd8c2d7003955733441de0521a885
Sanjit Mann <sumatmann@gmail.com>
2022-02-17 23:47:00 -0800

    works2

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-  connectionString:  process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString:  process.env.DATABASE_URL
 })
 
 var app = express()
